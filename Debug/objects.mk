b2c614c6cdec41324cbc9850edad76e6b79551d4
Cactus64k <cactus64k@yandex.ru>
2015-01-30 13:44:36 +0300

    Delete trash files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /home/cactus/Yandex.Disk/Eclipse/LINUX/simple-fb2-reader/Debug/gui/simple-fb2-reader.glade.o
-
-LIBS := -lgtk-3 -lxml2
-
