c2451e87841f0322cd0484b605b3936aa9638ad4
Cactus64k <cactus64k@yandex.ru>
2015-03-17 13:40:53 +0300

    Fix

--- cppcheck_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export INCLUDE=`pkg-config --cflags-only-I gtk+-3.0`
-export CFG_COUNT=1
-cppcheck -j8 -q --inconclusive --library=gtk --enable=all --std=c99 --report-progress --max-configs=$CFG_COUNT $INCLUDE ./src/
-echo '\nDone\n'
-read
--- src/callbacks/menubar_callbacks/about_callback.c
+++
@@ -1,9 +0,0 @@
-#include "../../chunks.h"
-
-void about_app_imagemenuitem_activate_cb(GtkMenuItem *menuitem, gpointer user_data)
-{
-	GtkAboutDialog* about_dialog	= GLOBAL_FB2_READER.about_dialog;
-	gtk_dialog_run(GTK_DIALOG(about_dialog));
-	gtk_widget_hide(GTK_WIDGET(about_dialog));
-
-}
