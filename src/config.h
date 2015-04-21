00a430d91b5fec07a81b7e22c4d1eab3a789d8fe
Cactus64k <cactus64k@yandex.ru>
2015-04-21 16:49:34 +0300

    Update version, refactoring, fix path generation

--- src/config.h
+++ src/config.h
@@ -1,5 +1,3 @@
 /* src/config.h.  Generated from config.h.in by configure.  */
-#define GUI_CONSTRUCT_PATH "NONE/share/simple-fb2-reader/simple-fb2-reader.glade"
-#define ENCODE_LIST_PATH "${prefix}/etc/simple-fb2-reader_encoding_list.cfg"
 #define PACKAGE_VERSION "1.0.6"
 #define PACKAGE_NAME "simple-fb2-reader"
